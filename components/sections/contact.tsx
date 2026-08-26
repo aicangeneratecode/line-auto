e8f90fb8ac90959ae41ffb0ee4dd6d43b11814dc
aicangeneratecode <sashamogool@gmail.com>
2026-08-26 11:45:11 +0200

    Обновил сайт3

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -106,7 +106,7 @@ export function Contact() {
         {/* Карта – правая половина */}
         <div className="relative h-64 lg:h-auto bg-gray-700 min-h-[250px] lg:min-h-[300px]">
           <iframe
-            src={COMPANY.googleMaps}
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1000!2d16.657426347260564!3d44.93063239871438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a6fd7a0434b35%3A0x5213f88bbd74fbbe!2sLINE-AUTO!5e1!3m2!1sru!2srs!4v1787736883753!5m2!1sru!2srs"
             className="absolute inset-0 w-full h-full"
             style={{ border: 0 }}
             allowFullScreen
